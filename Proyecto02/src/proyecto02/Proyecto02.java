e124d3892d7aa81cf59c6153e787964625770bc4
suanny jaramillo <sjmayon1@utpl.edu.ec>
2023-10-19 16:54:38 -0500

    tercer commit,trabajando  con git  y githubclassroom

---
+++ Proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto02;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("ejemplo dos basico de java versionado a traves de git manejado desde github ademas de usar githubclassroom");
+        
+        
+    }
+    
+}
